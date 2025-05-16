f4ce494b114e492145a1829b89e6ff47b88bc55a
Avineet Singh <127533961+avineet4@users.noreply.github.com>
2025-05-17 01:32:54 +0530

    Time: 0 ms (100.00%) | Memory: 21.5 MB (92.24%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        array<bitset<9>, 9> rows{}, cols{}, boxes{};
+
+        for(int i = 0; i < 9; ++i) {
+            for(int j = 0; j < 9; ++j) {
+                if(board[i][j] != '.') {
+                    int num = board[i][j] - '1';
+                    int boxIndex = (i / 3) * 3 + (j / 3);
+
+                    if(rows[i][num] || cols[j][num] || boxes[boxIndex][num]) {
+                        return false;
+                    }
+
+                    rows[i][num] = cols[j][num] = boxes[boxIndex][num] = true;
+                }
+            }
+        }
+
+        return true;
+    }
+};
