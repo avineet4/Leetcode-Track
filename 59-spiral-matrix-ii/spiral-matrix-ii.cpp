225ceaacea5be81b33a3a957b7a78d26b41de80b
Avineet Singh <127533961+avineet4@users.noreply.github.com>
2025-05-18 20:49:16 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (37.83%) - LeetSync

--- 59-spiral-matrix-ii/spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii/spiral-matrix-ii.cpp
@@ -8,33 +8,23 @@ public:
 
         int num = 1;
 
-        while (top<=bottom && left<=right) {
-            for (int i = left; i <= right; i++) {
+        while (top <= bottom && left <= right) {
+            for (int i = left; i <= right; ++i)
                 result[top][i] = num++;
-            }
             top++;
 
-            for (int i = top; i <= bottom; i++) {
+            for (int i = top; i <= bottom; ++i)
                 result[i][right] = num++;
-            }
             right--;
 
-            if (bottom < top) {
-                break;
-            }
-
-            for (int i = right; i >= left; i--) {
-                result[bottom][i] = num++;
-            }
+            for (int i = right; i >= left; --i)
+                if (top <= bottom)
+                    result[bottom][i] = num++;
             bottom--;
 
-            if(right < left) {
-                break;
-            }
-
-            for (int i = bottom; i >= top; i--) {
-                result[i][left] = num++;
-            }
+            for (int i = bottom; i >= top; --i)
+                if (left <= right)
+                    result[i][left] = num++;
             left++;
         }
 
