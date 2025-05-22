3df397a8b5190f5b439dc3ecbd492cdf554403a7
Avineet Singh <127533961+avineet4@users.noreply.github.com>
2025-05-22 09:52:55 +0530

    Time: 9 ms (70.51%) | Memory: 13 MB (79.90%) - LeetSync

--- 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int partitionString(string s) {
-        int n = s.size();
         vector<bool> group(26, 0);
-
         int ans = 0;
 
-        for(int i = 0; i < n; ++i) {
-            if(group[s[i] - 'a']){
+        for(const char& letter: s) {
+            if(group[letter - 'a']){
                 ans++;
                 fill(group.begin(), group.end(), 0);
             }
 
-            group[s[i] - 'a'] = 1;
+            group[letter - 'a'] = 1;
         }
 
         return ++ans;
