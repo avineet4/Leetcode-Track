4207d24e2dae8dbddfd90ee10203ac24dad81786
Avineet Singh <127533961+avineet4@users.noreply.github.com>
2025-06-24 20:38:40 +0530

    Time: 0 ms (100.00%) | Memory: 65.1 MB (65.60%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<int> inEdge(n + 1, 0), outEdge(n + 1, 0);
+        vector<int> edges(n + 1, 0);
 
         for(const auto& t : trust) {
-            outEdge[t[0]]++;
-            inEdge[t[1]]++;
+            edges[t[0]]--;
+            edges[t[1]]++;
         }
 
         // Now check judge condition
         for (int i = 1; i <= n; ++i) {
-            if (outEdge[i] == 0  && inEdge[i] == n - 1) {
+            if (edges[i] == n - 1) {
                 return i;
             }
         }
