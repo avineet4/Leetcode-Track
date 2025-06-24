9ae9c50822388b978fd95c1cfecd2add58c94a73
Avineet Singh <127533961+avineet4@users.noreply.github.com>
2025-06-24 20:35:20 +0530

    Time: 11 ms (46.29%) | Memory: 65.1 MB (65.60%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -1,48 +1,16 @@
-class Node {
-public:
-    int data;
-    Node* next;
-
-    Node() : data(0), next(nullptr) {}
-    Node(int val) : data(val), next(nullptr) {}
-};
-
 class Solution {
-
-    unordered_map<int, Node*> adjList;
-    int nodes = 0;
-    
-    void addEdge(int u, int v) {
-        if(adjList.find(u) == adjList.end()) {
-            adjList[u] = nullptr;
-            nodes++;
-        }
-
-        Node* newNodeU = new Node(v);
-        newNodeU->next = adjList[u];
-        adjList[u] = newNodeU;
-    }
-
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> inEdge(n + 1, 0), outEdge(n + 1, 0);
+
         for(const auto& t : trust) {
-            addEdge(t[0], t[1]);
-        }
-
-        // Count how many people trust each person
-        vector<int> trustedBy(n + 1, 0);
-
-        for (const auto& [person, head] : adjList) {
-            Node* temp = head;
-            while (temp) {
-                trustedBy[temp->data]++;
-                temp = temp->next;
-            }
+            outEdge[t[0]]++;
+            inEdge[t[1]]++;
         }
 
         // Now check judge condition
         for (int i = 1; i <= n; ++i) {
-            if (adjList.find(i) == adjList.end() && trustedBy[i] == n - 1) {
+            if (outEdge[i] == 0  && inEdge[i] == n - 1) {
                 return i;
             }
         }
