222f0e477efcbf522c585b085eae06d10934db97
Avineet Singh <127533961+avineet4@users.noreply.github.com>
2025-06-24 22:59:00 +0530

    Time: 6 ms (30.27%) | Memory: 9.9 MB (68.48%) - LeetSync

---
+++ 38-count-and-say/count-and-say.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        if(n == 1) return "1";
+
+        string result = "1";
+
+        for(int i = 2; i <= n; ++i) {
+            string local = "";
+            int count = 1;
+            for(int j = 1; j < result.size(); ++j) {
+                if(result[j] == result[j - 1]) {
+                    count++;
+                } else {
+                    local += to_string(count) + result[j - 1];
+                    count = 1;
+                }
+            }
+            local += to_string(count) + result.back();
+            result = local;
+        }
+
+        return result;
+    }
+};
