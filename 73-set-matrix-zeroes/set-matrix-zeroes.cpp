4505d270603e023ffdc71c40cd015b5478047edc
Avineet Singh <127533961+avineet4@users.noreply.github.com>
2025-06-27 23:29:42 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (50.95%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -4,30 +4,38 @@ public:
         int rows = matrix.size();
         int cols = matrix[0].size();
 
-        set<int> row;
-        set<int> col;
+        bool isFirstRow = false;
+        bool isFirstCol = false;
+
         for(int i = 0; i < rows; ++i) {
             for(int j = 0; j < cols; ++j) {
                 if(matrix[i][j] == 0) {
-                    row.insert(i);
-                    col.insert(j);
+                    if(i == 0) isFirstRow = true;
+                    else matrix[i][0] = 0;
+
+                    if(j == 0) isFirstCol = true;
+                    else matrix[0][j] = 0;
                 }
             }
         }
 
-        for(int i = 0; i < rows; ++i) {
-            if(row.find(i) != row.end()) {
-                for(int j = 0; j < cols; ++j) {
+        for(int i = 1; i < rows; ++i) {
+            for(int j = 1; j < cols; ++j) {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
             }
         }
 
-        for(int i = 0; i < cols; ++i) {
-            if(col.find(i) != col.end()) {
-                for(int j = 0; j < rows; ++j) {
-                    matrix[j][i] = 0;
-                }
+        if(isFirstRow) {
+            for(int i = 0; i < cols; ++i) {
+                matrix[0][i] = 0;
+            }
+        }
+
+        if(isFirstCol) {
+            for(int i = 0; i < rows; ++i) {
+                matrix[i][0] = 0;
             }
         }
 
