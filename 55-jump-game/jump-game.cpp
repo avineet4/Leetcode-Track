e49c94641678450e90319f7820f18ca869da6552
Avineet Singh <127533961+avineet4@users.noreply.github.com>
2025-06-30 03:02:32 +0530

    Time: 247 ms (15.19%) | Memory: 55.8 MB (6.32%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
-    enum State {
-        UNKNOWN,
-        GOOD,
-        BAD
-    };
+    enum State { UNKNOWN, GOOD, BAD };
 
     bool helper(vector<int>& nums, vector<State>& memo, int index) {
         if (index >= nums.size() - 1) {
@@ -16,8 +12,8 @@ public:
         }
 
         int maxJump = nums[index];
-        for(int i = 1; i <= maxJump; ++i){
-            if (helper(nums, memo, index + i)) {
+        for (int jump = maxJump; jump >= 1; --jump) {
+            if (helper(nums, memo, index + jump)) {
                 memo[index] = GOOD;
                 return true;
             }
