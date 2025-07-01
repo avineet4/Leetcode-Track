f6105902bc74d25cde85d895bd44d026f23f11ae
Avineet Singh <127533961+avineet4@users.noreply.github.com>
2025-07-02 00:14:45 +0530

    Time: 2 ms (41.30%) | Memory: 32.2 MB (46.19%) - LeetSync

--- 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.cpp
+++ 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
+    struct Comparator {
+        int k;
+        Comparator(int col) : k(col) {}
+        bool operator()(const vector<int>& a, const vector<int>& b) const {
+            return a[k] > b[k];
+        }
+    };
+
     vector<vector<int>> sortTheStudents(vector<vector<int>>& v, int k) {
-        sort(v.begin(), v.end(),[k](auto& a, auto& b) { 
-            return a[k] > b[k]; 
-        });
+        sort(v.begin(), v.end(), Comparator(k));
 
         return v;
     }
